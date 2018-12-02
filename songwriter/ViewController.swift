2fd96ef84b5750c1aed53dab9bc1a0d9b7abc5fc
hellyeah <hellyeah@hellyeahs-MacBook-Pro.local>
2018-12-02 13:09:45 -0800

    made expandable table views; now i just need to load the right data in:

--- songwriter/ViewController.swift
+++ songwriter/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDele
 
     var numberOfRecords:Int = 0
     
-    var tableViewData = [cellData]()'
+    var tableViewData = [cellData]()
     
 //    class Highlight: NSObject {
 //        var recordNumber: Int!
@@ -98,6 +98,12 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDele
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //later on we'll for loop iterate through user defaults to fill this information in
+        tableViewData = [cellData(opened: false, title: "Title1", sectionData: ["cell1", "cell2", "cell3"]),
+                        cellData(opened: false, title: "Title2", sectionData: ["cell1", "cell2", "cell3"]),
+                        cellData(opened: false, title: "Title3", sectionData: ["cell1", "cell2", "cell3"]),
+                        cellData(opened: false, title: "Title4", sectionData: ["cell1", "cell2", "cell3"])]
     
         //Setting up Session
         recordingSession = AVAudioSession.sharedInstance()
@@ -138,34 +144,95 @@ class ViewController: UIViewController, AVAudioRecorderDelegate, UITableViewDele
     
     //Setting up Table View
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return numberOfRecords
+    func numberOfSections(in tableView: UITableView) -> Int {
+        //return numberOfRecords
+        return tableViewData.count
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableViewData[section].opened == true {
+            return tableViewData[section].sectionData.count + 1
+        }
+        else {
+            return 1
+        }
+    }
+    
+    
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = String(indexPath.row + 1)
-        return cell
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+//        cell.textLabel?.text = String(indexPath.row + 1)
+        
+        
+        let dataIndex = indexPath.row - 1
+        
+        if indexPath.row == 0 {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell()}
+            cell.textLabel?.text = tableViewData[indexPath.section].title
+            return cell
+        }
+        else {
+            //use different cell identifier if needed
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell()}
+            cell.textLabel?.text = tableViewData[indexPath.section].sectionData[dataIndex]
+            return cell
+        }
+
+        //return cell
     }
     
     // listen to tap on one of the cells to playback
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let path = getDirectory().appendingPathComponent("\(indexPath.row + 1).m4a")
         
-        do {
-            audioPlayer = try AVAudioPlayer(contentsOf: path)
-            audioPlayer.play()
-            
-            if isKeyPresentInUserDefaults(key: "\(indexPath.row+1)") {
-                let highlights = UserDefaults.standard.array(forKey: "\(indexPath.row+1)") as! [Int]
-                print(highlights)
+        if indexPath.row == 0 {
+            if tableViewData[indexPath.section].opened == true {
+                tableViewData[indexPath.section].opened = false
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none) //play around with this
             }
-        }
-        catch {
+            else {
+                tableViewData[indexPath.section].opened = true
+                let sections = IndexSet.init(integer: indexPath.section)
+                tableView.reloadSections(sections, with: .none) //play around with this
+            }
+            
+            let path = getDirectory().appendingPathComponent("\(indexPath.row + 1).m4a")
+            
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: path)
+                audioPlayer.play()
+                
+                if isKeyPresentInUserDefaults(key: "\(indexPath.row+1)") {
+                    let highlights = UserDefaults.standard.array(forKey: "\(indexPath.row+1)") as! [Int]
+                    print(highlights)
+                }
+            }
+            catch {
+                
+            }
             
         }
+        else {
+            
+        }
+        
+
     }
-    
+//
+//    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+//        if tableViewData[indexPath.section].opened == true {
+//            tableViewData[indexPath.section].opened = false
+//            let sections = IndexSet.init(integer: indexPath.section)
+//            tableView.reloadSections(sections, with: .none) //play around with this
+//        }
+//        else {
+//            tableViewData[indexPath.section].opened = true
+//            let sections = IndexSet.init(integer: indexPath.section)
+//            tableView.reloadSections(sections, with: .none) //play around with this
+//        }
+//    }
+//
 }
 
